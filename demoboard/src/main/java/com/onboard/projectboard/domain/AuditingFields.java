0046e5707a6cf7a91ef0c4b43e075137a5cf87da
decemberiscoming <luckyctechmaster@gmail.com>
2023-09-12 23:51:38 +0900

    #12 - AudtingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로 발생하며,
    엔티티 클래스에 들어가는 요소이고,
    도메인 직접 연관이 없는 요소이므로, 추출가능
    
    '@MappedSuperclass' 이용하여 상속방식으로 추가.
    
    그 외 '@DateTimeFormat' 요소 추가 및 jpa 옵션 개

---
+++ demoboard/src/main/java/com/onboard/projectboard/domain/AuditingFields.java
@@ -0,0 +1,43 @@
+package com.onboard.projectboard.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter //앞 필드 접근을 위한 코드, 세터는 수동작동이므로 넣지 않는다. 해당 파일은 자동실행되어야 한다.
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class AuditingFields {
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt; //생성일시
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false, length = 100)
+    private String createdBy; // 생성자
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt; //수정일시
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
+    private String modifiedBy; //수정자
+
+}
+
